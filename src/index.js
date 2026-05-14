a3113e71803c00adb8501d7c3a545e1165a9b7c2
kayakazitapane <khayakazitapane@gmail.com>
2026-05-14 16:05:05 +0200

    Updated project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+
+  let context =
+    "You are a romantic poem expert who writes short English poems. You must generate a 20 -line poem in basic HTML and separate each line with a <br />. Follow user instructions. Do not include a title. Sign the poem with inside a <strong> element at the end of the poem and NOT at the beginning";
+
+  let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an English poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
